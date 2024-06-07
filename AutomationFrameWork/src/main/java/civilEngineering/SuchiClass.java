8343ed472d595d16e343fcc208bf0ef0c40ae695
Devanshu Shrivastava <Devanshu Shrivastava@DESKTOP-57E8I4B>
2024-06-07 10:45:38 +0530

    Suchi Class added

---
+++ AutomationFrameWork/src/main/java/civilEngineering/SuchiClass.java
@@ -0,0 +1,11 @@
+package civilEngineering;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SuchiClass {
+   @Test
+   public void test() {
+	   Reporter.log("Suchi class is added",true);
+   }
+}
