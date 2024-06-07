c1be3b5eb5be6e84a07f91259f120cdfd6f73504
suchi <suchi@SUCHITRA>
2024-06-07 10:42:38 +0530

    Suchi class is now added in devanshu

--- AutomationFrameWork/src/main/java/civilEngineering/SuchiClass.java
+++ AutomationFrameWork/src/main/java/civilEngineering/SuchiClass.java
@@ -6,6 +6,6 @@ import org.testng.annotations.Test;
 public class SuchiClass {
    @Test
    public void test() {
-	   Reporter.log("Suchi class is added",true);
+	   Reporter.log("Suchi class is now added in devanshu",true);
    }
 }
