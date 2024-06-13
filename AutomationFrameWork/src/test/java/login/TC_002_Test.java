48344a5f7bdaecede8f0c7df998700eba376fb1d
suchi <suchi@SUCHITRA>
2024-06-13 08:59:44 +0530

    TC_002_Test is executed

---
+++ AutomationFrameWork/src/test/java/login/TC_002_Test.java
@@ -0,0 +1,11 @@
+package login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC_002_Test {
+@Test
+public void test() {
+	Reporter.log("TC_002_Test is executed", true);
+}
+}
